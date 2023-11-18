e0918a292e1d8a8a2f7357bb8cb74e566aa8e23e
Robert Allen <robby.kirby@gmail.com>
2023-11-18 14:30:53 -0700

    Added get_arc_name_at function for use by tracker

--- scenes/arc_donut.gd
+++ scenes/arc_donut.gd
@@ -10,15 +10,15 @@ class_name ArcDonut;
 
 @export_category("Arcs")
 @export_range(0, 360) var start_angle: float = 0;
+@export var arc_names: Array[String] = ["yellow", "orange", "red"];
 @export var angle_sizes: Array[float] = [21.6, 10.8, 5.4];
 @export var colors: Array[Color] = [Color.html("#EE9B00"), Color.html("#CA6702"), Color.html("#AE2012")];
-
 
 func _process(_delta):
 	if Engine.is_editor_hint():
 		queue_redraw();
 
-func draw_arc_donut_about_point(center: Vector2, from_angle: float, to_angle: float, color: Color):
+func _draw_arc_donut_about_point(center: Vector2, from_angle: float, to_angle: float, color: Color):
 	var points_arc = PackedVector2Array()
 	var inner_radius = outer_radius * inner_to_outer_ratio;
 
@@ -30,16 +30,33 @@ func draw_arc_donut_about_point(center: Vector2, from_angle: float, to_angle: fl
 		var angle_point = deg_to_rad(from_angle + i * (to_angle - from_angle) / number_of_points - 90);
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * inner_radius);
 
-	draw_colored_polygon(points_arc, color)
+	draw_colored_polygon(points_arc, color);
+
+## Returns the name of the arc at _deg
+func get_arc_name_at(_deg: float):
+	var deg = fmod(_deg, 360);
+	print("deg ", deg)
+	if deg < start_angle:
+		return null;
+
+	var total_angle = start_angle;
+	for i in len(angle_sizes):
+		var angle = angle_sizes[i];
+		total_angle += angle;
+		print("angle ", total_angle)
+		if deg <= total_angle:
+			return arc_names[i];
+
+	return null;
 
 func _draw():
 	var angle_start = start_angle;
 	for i in len(angle_sizes):
 		var angle = angle_sizes[i];
-		draw_arc_donut_about_point(Vector2(0, 0),
+		_draw_arc_donut_about_point(Vector2(0, 0),
 			angle_start, angle_start + angle,
 			colors[i],
 		);
 		angle_start += angle;
 
-	draw_arc_donut_about_point(Vector2(0, 0), angle_start, 360 + start_angle, bg_color);
+	_draw_arc_donut_about_point(Vector2(0, 0), angle_start, 360 + start_angle, bg_color);
